85df6067f75f5efaf3fcd7260028214202c4c972
lashla <45652713+lashla@users.noreply.github.com>
2021-10-14 14:30:16 +0300

    Update main.kt

--- main.kt
+++ main.kt
@@ -1,17 +1,19 @@
 fun main() {
     val a = intArrayOf(121, 144, 19, 161, 19, 144, 19, 11)
     val b = intArrayOf(121, 14641, 20736, 361, 25921, 361, 20736, 361)
-    comp(a, b)
+    println(comp(a, b))
 }
 
 
 fun comp (a: IntArray?, b: IntArray?): Boolean{
     val aLength = a?.size
+    val bool: Boolean = true
     if (aLength != null) {
         for(i in 0..aLength.toInt()){
             if (b != null) {
-                if (!b.contains(i*i)) return false
+                if (!b.contains(i*i)) bool = false
             }
         }
     }
+retun bool
 }
